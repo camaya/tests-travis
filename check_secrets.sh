66d43f60f08dfcd3b71e5cc87de6f9d4642c1177
Cristhian Amaya <cristhian.amaya@typeform.com>
2018-11-12 14:27:37 +0100

    Add github key to gitleaks

--- check_secrets.sh
+++ check_secrets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker pull zricethezav/gitleaks
-docker container run --rm --name=gitleaks zricethezav/gitleaks --github-pr=https://github.com/$TRAVIS_REPO_SLUG/pull/$TRAVIS_PULL_REQUEST
+docker container run --rm --name=gitleaks -e GITHUB_TOKEN=$GITHUB_API_KEY zricethezav/gitleaks --github-pr=https://github.com/$TRAVIS_REPO_SLUG/pull/$TRAVIS_PULL_REQUEST
